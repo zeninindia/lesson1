89ac823c8b8ba14701604138efa6b358aba47e48
Olga Zenkina <zeninindia@gmail.com>
2025-12-29 04:12:30 +0300

    Update lesson_1_task_4.py

--- lesson_1_task_4.py
+++ lesson_1_task_4.py
@@ -1,20 +1,3 @@
-def Phone_Number(num):
-    print(num, end="")
-x=8
-Phone_Number(x)
-Phone_Number(x)
-x=0
-Phone_Number(x)
-Phone_Number(x)
-x=5
-Phone_Number(x)
-Phone_Number(x)
-Phone_Number(x)
-x=3
-Phone_Number(x)
-x=5
-Phone_Number(x)
-x=3
-Phone_Number(x)
-x=5
-Phone_Number(x)
+def print_greeting():
+    print("Привет, мир!")
+print_greeting()
